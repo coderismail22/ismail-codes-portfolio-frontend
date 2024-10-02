3ee5b4430c57a67a23c49daf552924089964192d
ismail <coderismail22@gmail.com>
2024-10-02 18:54:00 +0600

    add top banner

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/SharedPages/Navbar/Navbar";
 import Footer from "../pages/SharedPages/Footer/Footer";
+import { TopBanner } from "../components/TopBanner/TopBanner";
 
 const MainLayout = () => {
   return (
     <div>
+      <TopBanner />
       <Navbar />
       <Outlet />
       <Footer />
